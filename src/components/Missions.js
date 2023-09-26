37d95f8df6d6587bcafec60d762ae8d8e4357aae
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-09-26 16:28:49 +0500

    Add Join/Leave Mission functionality and Create table with Bootstrap

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,6 +1,11 @@
 import React, { useEffect } from 'react';
+import '../styles/missions.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchMissions } from '../redux/missions/missionsSlice';
+import {
+  fetchMissions,
+  joinMission,
+  leaveMission,
+} from '../redux/missions/missionsSlice';
 
 function Missions() {
   const missionsList = useSelector((state) => state.missions.missions);
@@ -9,16 +14,64 @@ function Missions() {
   useEffect(() => {
     dispatch(fetchMissions());
   }, [dispatch]);
+
+  const handleJoinMission = (id) => {
+    dispatch(joinMission(id));
+  };
+
+  const handleLeaveMission = (id) => {
+    dispatch(leaveMission(id));
+  };
   return (
     <div>
-      <h2>Missions</h2>
-      {missionsList.map((mission) => (
-        <div key={mission.id}>
-          <h5>{mission.mission_id}</h5>
-          <h3>{mission.mission_name}</h3>
-          <p>{mission.description}</p>
-        </div>
-      ))}
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Join</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missionsList.map((mission) => (
+            <tr key={mission.id}>
+              <td className="missionName">{mission.mission_name}</td>
+              <td>{mission.description}</td>
+              <td className="memberTd textCenter">
+                {mission.reserved ? (
+                  <span className="badge bg-info">Active Member</span>
+                ) : (
+                  <span className="badge bg-secondary">NOT A MEMBER</span>
+                )}
+              </td>
+              <td className="joinBtnTd textCenter">
+                {mission.reserved ? (
+                  <button
+                    type="button"
+                    className="btn text-nowrap btn-outline-danger leaveMissionBtn"
+                    onClick={() => {
+                      handleLeaveMission(mission.id);
+                    }}
+                  >
+                    Leave Mission
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className="btn text-nowrap btn-outline-dark joinMissioinBtn"
+                    onClick={() => {
+                      handleJoinMission(mission.id);
+                    }}
+                  >
+                    Join Mission
+                  </button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
