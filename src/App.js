68ff55d7db201cbcb9a7d7b2d0f32916f442fcb8
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-09-26 14:17:16 +0500

    Create Components, Add Routes and Add Navigation Links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import MyProfile from './components/MyProfile';
+
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/myprofile" element={<MyProfile />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
