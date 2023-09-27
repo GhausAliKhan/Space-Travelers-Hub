dd5c315aedf2b059d0c03d865c7c3084f3415138
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-09-27 11:44:46 +0500

    Display Rockets  Data using useSelector() Redux Hook

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import '../styles/rockets.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../redux/rockets/rocketsSlice';
 
 function Rockets() {
-  return <div>Rockets</div>;
+  const rocketsList = useSelector((state) => state.rockets.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+  return (
+    <div className="container my-4">
+      {rocketsList.map((rocket) => (
+        <div className="rocket-card my-4" key={rocket.id}>
+          <img
+            className="rocket-image"
+            src={rocket.flickr_images[0]}
+            alt={rocket.name}
+          />
+          <div className="rocket-info">
+            <h2>{rocket.name}</h2>
+            <p>{rocket.description}</p>
+            <button className="reserve-button" type="button">
+              Reserve Rocket
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Rockets;
